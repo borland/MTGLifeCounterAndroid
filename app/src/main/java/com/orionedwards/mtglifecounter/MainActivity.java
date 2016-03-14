2db269ca6b967306e5dadd7ff31f04ecf12a0e3e
Orion Edwards <orion.edwards@gmail.com>
2016-03-15 07:32:44 +1300

    Refactor main menu into a regular list

--- app/src/main/java/com/orionedwards/mtglifecounter/MainActivity.java
+++ app/src/main/java/com/orionedwards/mtglifecounter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.orionedwards.mtglifecounter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,55 +15,77 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        ListView menuList = (ListView)findViewById(R.id.main_menu_list);
+        menuList.setAdapter(new MenuListAdapter());
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+    class MenuListAdapter extends BaseAdapter {
+        final String[] mItems = new String[]{
+                "- Games",
+                "Duel",
+                "Two-headed Giant",
+                "3 Player",
+                "- Utilities",
+                "Roll D20"
+        };
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_duel) {
-            // Handle the camera action
-        } else if (id == R.id.nav_2hg) {
-
-        } else if (id == R.id.nav_3player) {
-
-        } else if (id == R.id.nav_d20) {
-
+        @Override
+        public int getCount() {
+            return mItems.length;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        @Override
+        public Object getItem(int position) {
+            return mItems[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)MainActivity.this.getApplicationContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            String item = mItems[position];
+
+            View rowView;
+
+            if(item.startsWith("- ")) { // header view
+                item = item.substring(2);
+                rowView = inflater.inflate(R.layout.menu_header, parent, false);
+            } else {
+                rowView = inflater.inflate(R.layout.menu_row, parent, false);
+            }
+
+            rowView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT));
+
+            TextView textView = (TextView) rowView.findViewById(R.id.text_view);
+            textView.setText(item);
+            return rowView;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            String item = mItems[position];
+            return !item.startsWith("- "); // disabled for headers
+        }
     }
 }
