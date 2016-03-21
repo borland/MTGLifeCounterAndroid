83c7c2851b57bc3ca3ffba143077efa7936d0e36
Orion Edwards <orion.edwards@gmail.com>
2016-03-22 06:24:28 +1300

    Implement basic up/down tracking and port more code from swift

--- app/src/main/java/com/orionedwards/mtglifecounter/DuelActivity.java
+++ app/src/main/java/com/orionedwards/mtglifecounter/DuelActivity.java
@@ -1,6 +1,7 @@
 package com.orionedwards.mtglifecounter;
 
 import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +9,15 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class DuelActivity extends FragmentActivity implements PlayerFragment.OnFragmentInteractionListener {
+public class DuelActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duel);
-    }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
+        PlayerFragment f = (PlayerFragment)getSupportFragmentManager().findFragmentById(R.id.duelP1fragment);
+        f.setIsUpsideDown(true);
     }
 
     public void onBackButtonClicked(View v) {
--- app/src/main/java/com/orionedwards/mtglifecounter/PlayerFragment.java
+++ app/src/main/java/com/orionedwards/mtglifecounter/PlayerFragment.java
@@ -3,106 +3,213 @@ package com.orionedwards.mtglifecounter;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.BoolRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.widget.TextViewCompat;
+import android.util.Log;
+import android.view.Display;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Callable;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PlayerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PlayerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PlayerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class PlayerFragment extends Fragment implements View.OnTouchListener, GestureDetector.OnGestureListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View mView;
+    private TextView mLifeTotalLabel;
 
-    private OnFragmentInteractionListener mListener;
+    private final GestureDetectorCompat mGestureDetector;
+
+    private LifeTotalDeltaTracker mTracker = new LifeTotalDeltaTracker();
+    private String mPlayerName = "";
+    private int mLifeTotal = 0;
+    private Boolean mIsUpsideDown = false;
+    private MtgColor mColor = MtgColor.White;
 
     public PlayerFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlayerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlayerFragment newInstance(String param1, String param2) {
-        PlayerFragment fragment = new PlayerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        mGestureDetector = new GestureDetectorCompat(this.getContext(), this);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false);
-    }
+        mView = inflater.inflate(R.layout.fragment_player, container, false);
+        mView.setOnTouchListener(this);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        mLifeTotalLabel = (TextView)mView.findViewById(R.id.lifeTotal);
+        return mView;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    // ----- Logic -----
+
+    public int getLifeTotal() {
+        return mLifeTotal;
     }
+
+    public void setLifeTotal(int value) {
+        mLifeTotal = value;
+        mTracker.update(value);
+
+        mLifeTotalLabel.setText(String.format("%d",mLifeTotal));
+    }
+
+    public MtgColor getColor() {
+        return mColor;
+    }
+
+    public void setColor(MtgColor value) {
+        mColor = value;
+    }
+
+    public boolean getIsUpsideDown() {
+        return mIsUpsideDown;
+    }
+
+    public void setIsUpsideDown(boolean value) {
+        mIsUpsideDown = value;
+        if(value) {
+            mView.setRotation(180);
+        }
+    }
+
+    // ----- Gesture Detecting -----
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) { }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+
+        boolean up = true;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+            case Surface.ROTATION_180: // phone in portrait - +/- buttons are on the side of the text
+                up = e.getX() > (mView.getMeasuredWidth() / 2);
+                break;
+
+            default: // landscape, +/- buttons are above and below the text
+                up = e.getY() < (mView.getMeasuredHeight() / 2);
+                break;
+        }
+
+        if(up) {
+            setLifeTotal(getLifeTotal() + 1);
+        } else {
+            setLifeTotal(getLifeTotal() - 1);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.i("onScroll", String.format("dX = %f, dY = %f", distanceX, distanceY));
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) { }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+}
+
+class HistoryTuple {
+    public Date when;
+    public int lifeTotal;
+
+    HistoryTuple(int lifeTotal) {
+        this.lifeTotal = lifeTotal;
+        this.when = new Date();
+    }
+}
+
+class LifeTotalDeltaTracker {
+    final int FLOATING_VIEW_FONT_SIZE = 44;
+    final double FLOATING_VIEW_TIMEOUT = 1.7;
+    final double FLOATING_VIEW_HIDE_TIME = 0.25;
+
+    private int mBaseline = 0; // we show +/- x relative to this
+    private LinkedList<HistoryTuple> mHistory = new LinkedList<>();
+    private View mFloatingView = null;
+    private Callable mCancelPreviousDelay = null;
+    private View mParent = null;
+
+    LifeTotalDeltaTracker() {
+
+    }
+
+    public View getParent() {
+        return mParent;
+    }
+
+    public void setParent(View parent) {
+        mParent = parent;
+    }
+
+    public void update(int lifeTotal) {
+        if(!mHistory.isEmpty()) {
+            HistoryTuple t = mHistory.getLast();
+            if(t.lifeTotal == lifeTotal) {
+                return; // no point recording a duplicate
+            }
+        }
+
+        HistoryTuple nt = new HistoryTuple(lifeTotal);
+        mHistory.addLast(nt);
+        updateUi(lifeTotal);
+    }
+
+    public void reset(int lifeTotal) {
+        mHistory.clear();
+        mBaseline = lifeTotal;
+        updateUi(lifeTotal);
+    }
+
+    private void updateUi(int lifeTotal) {
+
+    }
+
 }
